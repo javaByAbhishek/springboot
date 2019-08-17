aedb2bb1747c778d30e13e97ac3459585c5cdd9b
mahto.a[Abhishek] <mahto.a@LP-5CD809B9Z6.HCLT.CORP.HCL.IN>
2019-08-17 17:15:58 +0530

    First commit

---
+++ src/main/java/com/springboot/restservices/SpringbootRestservicesApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestservicesApplication.class, args);
+	}
+
+}
